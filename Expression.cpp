fc9fdbab798e32a363b01d8a2745fe876a49783b
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-03-18 11:00:21 +0530

    Update Expression.cpp

--- Expression.cpp
+++ Expression.cpp
@@ -1,26 +1,4 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#define lli               long long int
-#include <utility>
-#define pb                push_back
-#define ppb               pop_back
-#define pf                push_front
-#define ppf               pop_front
-#define all(x)            (x).begin(),(x).end()
-#define uniq(v)           (v).erase(unique(all(v)),(v).end())
-#define sz(x)             (int)((x).size())
-#define fr                first
-#define sc                second
-#define pii               pair<int,int>
-#define rep(i,a,b)        for(int i=a;i<b;i++)
-#define mem1(a)           memset(a,-1,sizeof(a))
-#define mem0(a)           memset(a,0,sizeof(a))
-#define ppc               __builtin_popcount
-#define ppcll             __builtin_popcountll
-using namespace std;
- 
- 
- 
+
 void solve(){
   int a,b,c;
   cin>>a>>b>>c;
@@ -41,12 +19,7 @@ signed main(){
 #ifndef ONLINE_JUDGE
 freopen("input1.txt", "r", stdin);
 freopen("output1.txt", "w", stdout);
-#endif
- 
- 
- 
-  solve();
-  
- 
+#endif 
+  solve(); 
 return 0;
 }
