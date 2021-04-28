23304172755d36f0c25e6b1adb9d6761041c9b23
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-04-28 22:10:15 +0530

    Create Little Pony and Sort by Shift.cpp

---
+++ Little Pony and Sort by Shift.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/454/B
+
+
+
+
+void solve(){
+     
+    
+    int n, s, v(0);
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n - 1; i++) if (a[i] > a[i + 1]) s = i, v++;
+    if (a[n - 1] > a[0]) s = n - 1, v++;
+    if (v == 0) cout << 0 << endl;
+    else if (v > 1) cout << -1 << endl;
+    else cout << n - 1 - s << endl;
+
+           
+}
