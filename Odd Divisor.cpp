bba1a1c8926b86c99ea894f761f337c1e45da149
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-05-26 14:26:03 +0530

    Create Odd Divisor.cpp

---
+++ Odd Divisor.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/1475/problem/A
+
+
+
+ 
+void solve() {
+   
+   lli n;
+   cin >> n;
+   
+   if(n&(n-1)){
+     cout << "YES" <<"\n";
+    }else{
+        cout << "NO" <<"\n";
+    }
+      
+}
