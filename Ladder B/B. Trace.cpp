2058ca7d044d3bd499aa84445fb772d35d43bea2
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-08-30 23:45:58 +0530

    B. Trace

---
+++ Ladder B/B. Trace.cpp
@@ -0,0 +1,27 @@
+https://codeforces.com/problemset/problem/157/B
+
+
+void solve(){
+   int n;
+   cin >> n;
+   double a[n+10];
+   for(int i = 0 ; i < n ; i++){
+    cin >> a[i];
+   }
+   sort(a,a+n);
+   bool flag = true;
+   double value = 0;
+   
+   for(int i = n-1 ; i >= 0 ; i--){
+      if(flag){
+        value += (a[i]*a[i]);
+        flag = false;
+      }
+      else{
+        flag = true;
+        value -= (a[i] * a[i]);
+      }
+   }
+   double ans = pi * value;
+   cout << ans <<"\n";
+}
